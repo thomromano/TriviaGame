cf0feffc45eccd4a442f0787ed338add651b5e85
thomromano <thomromano@gmail.com>
2018-03-16 15:30:21 -0500

    html scaffolding and begin .js

---
+++ assets/javascript/app.js
@@ -0,0 +1,32 @@
+'use strict';
+
+//initialize .js
+$(document0).ready(function(){
+//initialize button to start game
+//create object to hold all questions and answers
+//event to start timer
+
+//create form for questions
+//for loop for answers array
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
